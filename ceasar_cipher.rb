2e0018f2e58c09a06457d7bd9ec4f24fd007e5ca
Ghassan Taha <ghassan.m.taha@gmail.com>
2018-11-22 17:43:36 -0700

    fixed multi word bug

--- ceasar_cipher.rb
+++ ceasar_cipher.rb
@@ -1,21 +1,23 @@
-def ceasar_cipher(text, shift)
+def caesar_cipher(text, shift)
 	print text + " Shifted by " + shift.to_s + ": "
 	internal_shift = shift % 26
 	for i in  0..(text.length-1)
 		if (text[i].ord.to_i >= 65 && text[i].ord.to_i <= 90) then
 			base = 65;
+			text[i] = (base +(((text[i].ord.to_i + internal_shift)%base)%26)).chr
 		elsif (text[i].ord.to_i >= 97 && text[i].ord.to_i <= 122) then
 			base = 97
+			text[i] = (base +(((text[i].ord.to_i + internal_shift)%base)%26)).chr
 		end
-		text[i] = (base +(((text[i].ord.to_i + internal_shift)%base)%26)).chr
 	end
 	puts text
 end
-ceasar_cipher("ABCD", 2)
-ceasar_cipher("abcd", 2)
-ceasar_cipher("wxyz", 2)
-ceasar_cipher("WXYZ", 2)
-ceasar_cipher("ABCD", 22)
-ceasar_cipher("abcd", 22)
-ceasar_cipher("wxyz", 22)
-ceasar_cipher("WXYZ", 55)
+caesar_cipher("ABCD", 2)
+caesar_cipher("abcd", 2)
+caesar_cipher("wxyz", 2)
+caesar_cipher("WXYZ", 2)
+caesar_cipher("ABCD", 22)
+caesar_cipher("abcd", 22)
+caesar_cipher("wxyz", 22)
+caesar_cipher("WXYZ", 55)
+caesar_cipher("What a string!", 5)
